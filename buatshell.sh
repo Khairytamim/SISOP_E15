da274ca46450a5173c59646f6d0c4a65cd24baab
khairytamim <fikry.labsky08@gmail.com>
2015-10-31 12:14:02 +0700

    deklarasi soal

---
+++ buatshell.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#bisa berpindah direktori aktif
+#menjalankan perintah yang ada di /bin dan usr/bin
+#tidak berhenti saat crts z+ ctrl c
+#ctrl+d = keluar
+#bisa menjadalankan perintah secara background (?)
+#no system function
