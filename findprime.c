f939f6bec1e2b5a54b3e5017f75720f26e309c0a
khairytamim <fikry.labsky08@gmail.com>
2015-11-01 17:12:54 +0700

    pengubahan flag agar diproses, tapi masih salah hasilnya

--- findprime.c
+++ findprime.c
@@ -6,19 +6,19 @@ void *prime1(void *args)
 {
 	int i,j,*cek;
 	cek=(int *)args;
+	int flag=0;
 	for(i=2; i<=*cek; i++)
 	{
-		int flag=0;
 		for(j=1; j<=i;j++)
 		{
 			if(i%j==0)
 			flag++;
 		}
-		if(flag==2)
-		{
-			temp++;
-			//return temp;
-		}
+	}
+	if(flag==2)
+	{
+		temp++;
+		//return temp;
 	}	
 }
 
@@ -29,9 +29,9 @@ void main ()
 	scanf("%d", &data);
 	for(loop=1;loop<data;loop++)
 	{
-		pthread_create(&t1, NULL, prime1,(void *)&loop);
+		pthread_create(&t1, NULL, prime1,(void*)&loop);
 		pthread_join(t1,NULL);
 	}
-	printf("%d", temp);
+	printf("%d ", temp);
 }
 
