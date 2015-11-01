28b8fdab77851b8b2251c14a259b466b40df4dff
zahrahcitra <zahrahcitra@yahoo.co.id>
2015-11-02 01:42:33 +0700

    memperbagus tampilan isi program

--- findprime.c
+++ findprime.c
@@ -5,7 +5,6 @@ int  temp=0;
 void *prime1(void *args)
 {
 	int i,j,*cek,flag=0;
-	//printf("nilai  = flag %d\n",flag);
 	cek=(int *)args;
 	for(i=2; i<=*cek; i++)
 	{
@@ -15,13 +14,9 @@ void *prime1(void *args)
 			if(i%j==0)
 			flag++;
 		}
-		//printf("nilai flag %d\n",flag);
 		if(flag==2)
 		{
-			//flag=0;
 			temp++;
-			//printf("%d tempnya adalah %d\n",*cek,temp);
-			//return temp;
 		}
 	}
 }
@@ -29,11 +24,10 @@ void *prime1(void *args)
 void main ()
 {
 	pthread_t t1; 
-	int data,loop;
+	int data;
 	printf("INPUT LIMIT: ");
 	scanf("%d", &data);
 	pthread_create(&t1, NULL, prime1,(void*)&data);
 	pthread_join(t1,NULL);
 	printf("JUMLAH BILANGAN PRIMA SEBELUM %d = %d\n",data, temp);
 }
-
