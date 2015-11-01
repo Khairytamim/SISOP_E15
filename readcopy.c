d8e0e3fe7e597e4c5b0974868ff1bcfe238db8ae
zahrahcitra <zahrahcitra@yahoo.co.id>
2015-11-01 22:18:04 +0700

    update copy file

--- readcopy.c
+++ readcopy.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void *readdata (void *args)
-{
+void *thread1(void *args){
+	FILE *in, *out;
+	char karakter;
+	in=fopen ("contoh.txt", "r");
+	out=fopen ("contoh2.txt", "w");
+	while ((karakter=fgetc (in))!=EOF)
+	{
+		(fputc(karakter, out));
+	}
+	fclose (in);
+	fclose (out);
 	
 }
 
-void *copydata (void *args)
-{
+void *thread2(void *args){
+	FILE *in, *out;
+	char karakter;
+	in=fopen ("contoh2.txt", "r");
+	out=fopen ("contoh3.txt", "w");
+	while ((karakter=fgetc (in))!=EOF)
+	{
+		(fputc (karakter, out));
+	}
+	fclose (in);
+	fclose (out);
 	
 }
 
-void main ()
-{
-	pthread_t task1, task2;
-	
-	
+int main(){
+	pthread_t t1, t2;
+	pthread_create (&t1, NULL, thread1, NULL);	
+	pthread_join (t1, NULL);
+	pthread_create (&t2, NULL, thread2, NULL);
+	pthread_join (t2, NULL);
 }
-
