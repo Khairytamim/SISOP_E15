eaf5e09aae3ae7acad65cfe1bf837a945943a161
khairytamim <fikry.labsky08@gmail.com>
2015-11-10 08:58:02 +0700

    oembuatan perintah cd dan exit menggunakan if

---
+++ makeashell.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#define chdir _chdir
+
+#else
+#include <unistd.h>
+#endif
+
+#define MAX_LENGTH 1024
+#define DELIMS " \t\r\n"
+
+
+/* 	menjalankan perintah-perintah usr/bin dan bin
+	tidak berhenti saat ctrlc+ctlrz
+	bisa menjalankan beberapa perintah secara background jika belakang perintah ada &
+	without system function
+*/
+int main(int argc, char *argv[]) 
+{
+  	char *cmd;
+  	char line[MAX_LENGTH];
+	while (1) 
+	{
+    		printf("masukkan perintah ");
+    		if (!fgets(line, MAX_LENGTH, stdin)) break;
+
+    		if ((cmd = strtok(line, DELIMS))) 
+		{
+      			errno = 0;
+
+     			if (strcmp(cmd, "cd") == 0) 
+			{
+        			char *arg = strtok(0, DELIMS);
+
+        			if (!arg) fprintf(stderr, "cd missing argument.\n");
+        			else chdir(arg);
+      			} 
+			else if (strcmp(cmd, "exit") == 0) 
+			{
+        			break;
+    			} 
+
+      			if (errno) perror("Command failed");
+    		}
+	}
+	printf("\n");	
+	return 0;
+}
