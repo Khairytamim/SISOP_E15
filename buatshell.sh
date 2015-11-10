1fb773c449fda4e2a26d04c095fac742659ce373
zahrahcitra <zahrahcitra@yahoo.co.id>
2015-11-10 20:25:35 +0700

    Delete buatshell.sh

--- buatshell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#bisa berpindah direktori aktif
-#menjalankan perintah yang ada di /bin dan usr/bin
-#tidak berhenti saat crts z+ ctrl c
-#ctrl+d = keluar
-#bisa menjadalankan perintah secara background (?)
-#no system function
