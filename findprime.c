f6ada39c7eebe5bfba10fdce73a386d5587a982b
khairytamim <fikry.labsky08@gmail.com>
2015-11-11 16:12:56 +0700

    penggunaan multithread

--- findprime.c
+++ findprime.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 #include <pthread.h>
 
-int  temp=0;
+int  temp;
 void *prime1(void *args)
 {
-	int i,j,*cek,flag=0;
-	cek=(int *)args;
-	for(i=2; i<=*cek; i++)
+	int i,cek,flag=0;
+	cek=*((int *)args);
+	for(i=1; i<=cek; i++)
 	{
-		flag=0;
-		for(j=1; j<=i;j++)
-		{
-			if(i%j==0)
+			if(cek%i==0)
 			flag++;
-		}
-		if(flag==2)
-		{
-			temp++;
-		}
 	}
+	if(flag==2)
+	temp++;
 }
 
 void main ()
 {
-	pthread_t t1; 
-	int data;
+	int data,i;
 	printf("INPUT LIMIT: ");
 	scanf("%d", &data);
-	pthread_create(&t1, NULL, prime1, (void *)&data);
-	pthread_join(t1,NULL);
+	pthread_t t1[data];
+	int input[data];
+	for(i=2;i<data;i++)
+	{
+		input[i]=i;
+		pthread_create(&t1[i], NULL, prime1,&input[i]);
+	}
+	for(i=2; i<data; i++)
+	pthread_join(t1[i],NULL);
+
 	printf("JUMLAH BILANGAN PRIMA SEBELUM %d = %d\n",data, temp);
 }
